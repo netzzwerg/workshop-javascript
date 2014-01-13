89c9a69add86efa972ce5c4949bcbed0e95aaf5c
Béla Varga <netzzwerg@googlemail.com>
2014-01-13 11:03:34 +0100

    Updated example02 (workflow with grunt).

---
+++ workflow-yeoman/grunt/example02/Gruntfile.js
@@ -0,0 +1,45 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    jshint: {
+      src: {
+        options: {
+          curly: false,
+          undef: true
+        },
+        files: {
+          src: ['app/src/app.js', 'app/src/lib.js']
+        }
+      }
+    },
+
+    concat: {
+      build: {
+        options: {
+          separator: ';',
+        },
+        src: ['app/src/jquery.js' ,'app/src/app.js', 'app/src/lib.js'],
+        dest: 'app/build/app.js'
+      }
+    },
+
+    uglify: {
+      build: {
+        files: { 'app/build/app.min.js': [ 'app/build/app.js' ] }
+      }
+    }
+
+  });
+
+  // Load task modules.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task.
+  grunt.registerTask('default', 'jshint');
+
+};
