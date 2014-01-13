915d649ee8711729c35b0240f0bda795e41c241f
Béla Varga <netzzwerg@googlemail.com>
2014-01-13 14:38:11 +0100

    Added example04 (workflow with grunt).

---
+++ workflow-yeoman/grunt/example04/Gruntfile.js
@@ -0,0 +1,45 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          base: '../app',
+          hostname: 'localhost', // Change to 0.0.0.0 to external connection.
+          open: true,
+          middleware: function (connect, options) {
+            return [
+              require('connect-livereload')({ port: 35729 }),
+              // Serve static files.
+              connect.static(options.base),
+            ];
+          }
+        }
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: [ '../app/src/*.js' , '../app/index.html' ],
+        tasks: [], // Add more tasks here.
+        options: {
+          livereload: 35729
+        },
+      },
+    }
+
+  });
+
+  // Load task modules.
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', 'server');
+  grunt.registerTask('server', ['connect', 'watch']);
+
+};
